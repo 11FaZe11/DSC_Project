307362ec38d7a740ca533156f68d0a32900f61b7
aisha <karamaisha695@gmail.com>
2025-03-18 14:22:18 +0200

    aisha branch creating

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-def print_message(message: str = "Hello, World!") -> None:
-    """
-    Prints a custom message to the console.
-
-    Parameters:
-    message (str): The message to be printed. Defaults to "Hello, World!".
-
-    Returns:
-    None
-    """
-    print(message)
+print("aisha")
